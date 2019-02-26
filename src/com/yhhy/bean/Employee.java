201be0d248e8dbaca5d4eb873ef8c874fd11c296
ééguoxianï <15001049264@163.com>
2019-02-26 13:56:34 +0800

    修改员工实体类

--- src/com/yhhy/bean/Employee.java
+++ src/com/yhhy/bean/Employee.java
@@ -7,11 +7,15 @@ public class Employee {
     /**
      * 员工ID
      */
-    private int id;
+    private int emp_id;
     /**
      * 员工姓名
      */
-    private String lastname;
+    private String emp_name;
+    /**
+     * 员工身份证号
+     */
+    private String id_code;
     /**
      * 员工邮箱
      */
@@ -19,30 +23,59 @@ public class Employee {
     /**
      * 员工性别
      */
-    private int gender;
+    private int sex;
+    /**
+     * 所属部门
+     */
+    private Department department;
 
-    public void setId(int id) {
-        this.id = id;
+
+    public int getEmp_id() {
+        return emp_id;
     }
-    public int getId() {
-        return id;
+
+    public void setEmp_id(int emp_id) {
+        this.emp_id = emp_id;
     }
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+
+    public String getEmp_name() {
+        return emp_name;
     }
-    public String getLastname() {
-        return lastname;
+
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
     }
-    public void setEmail(String email) {
-        this.email = email;
+
+    public String getId_code() {
+        return id_code;
     }
+
+    public void setId_code(String id_code) {
+        this.id_code = id_code;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+
     public String getEmail() {
         return email;
     }
-    public void setGender(int gender) {
-        this.gender = gender;
+
+    public void setEmail(String email) {
+        this.email = email;
     }
-    public int getGender() {
-        return gender;
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
